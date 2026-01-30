c5e59c335eb6002d737e6efe692c585576187b18
MAHALAKSHMI150506 <mahalakshmikan1505@gmail.com>
2026-01-30 16:01:19 +0530

    Update for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use("/api/journal", journalRoutes);
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Handle React routing, return all requests to React app
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
